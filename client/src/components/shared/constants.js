28092c007d8e2f77e097bff00809796815155a38
Pierce Butler <Pierce@Melissas-MacBook-Pro.local>
2020-03-11 09:56:23 -0400

    api calls

--- client/src/components/shared/constants.js
+++ client/src/components/shared/constants.js
@@ -3,3 +3,51 @@ const input = "batman begins"
 const explore = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
 const search = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${input}`
 export default explore
+
+class Home extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      movies: []
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get(explore)
+      console.log(response)
+      this.setState({
+        movies: response.data
+      })
+    }
+    catch (error) {
+      alert("error")
+    }
+  }
+  render() {
+    console.log(this.state.movies)
+
+
+    let flicks = this.state.movies.length !== 0 && this.state.movies.results.map((result, index) => {
+
+console.log(this.state.movies.results)
+      return (
+        <div key={index}>
+          {/* need to figure out a way to add the correct "intro" portion to the img call, so for now I wouldnt worry about actually getting the image */}
+          <img src={result.backdrop_path}/>
+          <h2>{result.title}</h2>
+          <p>{result.overview}</p>
+          <button onClick>Favorite</button>
+        </div>
+      )
+    })
+    return (
+      <>
+        <h1>flicks</h1>
+        {flicks}
+      </>
+    )
+  }
+}
+export default Home
